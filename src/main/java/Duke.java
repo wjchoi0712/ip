21e95d63db93051d18f43df808d8e4e35261da72
wjchoi0712 <e0376605@u.nus.edu>
2021-01-26 19:24:52 +0800

    Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,27 @@ import java.util.Scanner;
 
 public class Duke {
 
-    //Prints out the input with line surrounding it
-    public static void printWithLine(String message, String tasks[]) {
+    /**
+     * Prints out the arguments with LINE_SEPARATOR surrounding it
+     *
+     * @param message The string message to be printed out
+     * @param tasks The list of tasks to be printed out
+     * @return No return value.
+     */
+    public static void printWithLine(String message, Task[] tasks) {
         final String LINE_SEPARATOR = "____________________________________________________________";
         if (tasks == null) {
             System.out.println(LINE_SEPARATOR + "\n" + message+ "\n" + LINE_SEPARATOR);
         } else {
             System.out.println(LINE_SEPARATOR);
+            if (message != null) {
+                System.out.println(message);
+            }
             for (int i = 0; i < tasks.length; i++) {
                 if (tasks[i] == null) {
                     break;
                 }
-                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                System.out.println(" " + (i + 1) + "." + tasks[i].getStatusAndDescription());
             }
             System.out.println(LINE_SEPARATOR);
         }
@@ -30,15 +39,21 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCounter = 0;
 
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
-                printWithLine(null, tasks);
-            } else{
-                tasks[taskCounter] = userInput;
-                printWithLine(" added: " + tasks[taskCounter], null);
+                printWithLine(" Here are the tasks in your list:", tasks);
+            } else if (userInput.startsWith("done")) {
+                String[] temp = userInput.split(" ");
+                int completedTaskNo = Integer.parseInt(temp[1])-1;
+                tasks[completedTaskNo].markAsDone();
+                printWithLine(" Nice! I've marked this task as done:" + "\n   "
+                        + tasks[completedTaskNo].getStatusAndDescription(), null);
+            } else {
+                tasks[taskCounter] = new Task(userInput);
+                printWithLine(" added: " + tasks[taskCounter].description, null);
                 taskCounter++;
             }
             userInput = in.nextLine();
