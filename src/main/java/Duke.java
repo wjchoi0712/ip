fe9940fb51d01ac216881642bfbbeefa8075f29d
wjchoi0712 <e0376605@u.nus.edu>
2021-01-26 13:59:35 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    //Prints out the argument with line surrounding it
+    public static void printWithLine(String str) {
+        String line = "\t____________________________________________________________";
+        System.out.println(line + System.lineSeparator() + "\t " + str + System.lineSeparator() + line);
+    }
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line + "\n Hello! I'm Duke\n" +
-                " What can I do for you?\n" + line);
-        System.out.println(" Bye. Hope to see you again soon!\n" + line);
+        printWithLine("Hello! I'm Duke" + System.lineSeparator() + "\t What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while (!line.equals("bye")) {
+            printWithLine(line);
+            line = in.nextLine();
+        }
+        printWithLine("Bye. Hope to see you again soon!");
     }
 }
