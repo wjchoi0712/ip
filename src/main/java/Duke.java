afb108c6bca36abedf0aa779c7e7f374bbfa0d28
wjchoi0712 <e0376605@u.nus.edu>
2021-01-26 17:57:02 +0800

    Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,47 @@ import java.util.Scanner;
 
 public class Duke {
 
-    //Prints out the argument with line surrounding it
-    public static void printWithLine(String str) {
-        String line = "\t____________________________________________________________";
-        System.out.println(line + System.lineSeparator() + "\t " + str + System.lineSeparator() + line);
+    //Prints out the input with line surrounding it
+    public static void printWithLine(String message, String tasks[]) {
+        final String LINE_SEPARATOR = "____________________________________________________________";
+        if (tasks == null) {
+            System.out.println(LINE_SEPARATOR + "\n" + message+ "\n" + LINE_SEPARATOR);
+        } else {
+            System.out.println(LINE_SEPARATOR);
+            for (int i = 0; i < tasks.length; i++) {
+                if (tasks[i] == null) {
+                    break;
+                }
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+            System.out.println(LINE_SEPARATOR);
+        }
     }
 
     public static void main(String[] args) {
-        printWithLine("Hello! I'm Duke" + System.lineSeparator() + "\t What can I do for you?");
+        final String LOGO = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        printWithLine(LOGO + "\n Hello! I'm Duke" + "\n What can I do for you?", null);
+
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        while (!line.equals("bye")) {
-            printWithLine(line);
-            line = in.nextLine();
+        String userInput = in.nextLine();
+
+        String[] tasks = new String[100];
+        int taskCounter = 0;
+
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                printWithLine(null, tasks);
+            } else{
+                tasks[taskCounter] = userInput;
+                printWithLine(" added: " + tasks[taskCounter], null);
+                taskCounter++;
+            }
+            userInput = in.nextLine();
         }
-        printWithLine("Bye. Hope to see you again soon!");
+        printWithLine(" Bye. Hope to see you again soon!", null);
     }
 }
